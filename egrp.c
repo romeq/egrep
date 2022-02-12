450767821b0b7fac299b75b540bcb08e8f7f582b
romeq <me@romeq.fi>
2022-02-12 12:57:15 +0200

    Improve speed and verbose mode, cleanup a bit.

--- egrp.c
+++ egrp.c
@@ -46,70 +46,69 @@ main(int argc, char *argv[])
     }
 
     DIR *proc = opendir("/proc");
-    if (proc == NULL) { 
+    if (!proc) { 
         perror("/proc");
         return EXIT_FAILURE; 
     }
 
+    char line[128];
+    char link_path[128];
+    char real_username[128];
+    char link_realpath[128];
+    char status_file_path[128];
+    char proc_current_item[128];
+    FILE *status_file;
     struct dirent *dirent;
     while ((dirent = readdir(proc)) != NULL) {
         if (dirent->d_name[0] == '.') continue;
 
+        status_file = NULL;
+
         int pid = atoi(dirent->d_name); 
         if (pid == 0) continue;
 
-        char *line = malloc(256);
-        char *link_path = malloc(256);
-        char *link_realpath = malloc(256);
-        char *proc_file_path = malloc(256);
-        char *status_file_path = malloc(256);
-        FILE *status_file = NULL;
-
-        snprintf(proc_file_path, 255, "/proc/%s", dirent->d_name);
+        snprintf(proc_current_item, 127, "/proc/%s", dirent->d_name);
 
         struct stat item_stat;
-        if (stat(proc_file_path, &item_stat) < 0) {
-            perror("stat");
+        if (stat(proc_current_item, &item_stat) < 0) {
+            if (flag_verbose) perror("stat");
             goto end;
         }
         if ((item_stat.st_mode & S_IFMT) != S_IFDIR) goto end;
 
-        snprintf(link_path, 255, "/proc/%s/exe", dirent->d_name);
+        snprintf(link_path, 127, "/proc/%s/exe", dirent->d_name);
         if (!realpath(link_path, link_realpath)) goto end;
 
-        snprintf(status_file_path, 255, "/proc/%s/status", dirent->d_name);
+        snprintf(status_file_path, 127, "/proc/%s/status", dirent->d_name);
         status_file = fopen(status_file_path, "r");
         if (!status_file) {
-            perror(status_file_path); 
+            if (flag_verbose) perror(status_file_path); 
             goto end;
         }
         
-        if (filter_user != NULL) {
+        if (filter_user || flag_verbose) {
             int uid = 0;
-            while (fgets(line, 256, status_file) != NULL) {
+            while (fgets(line, 128, status_file) != NULL) {
                 if (sscanf(line, "Uid:%*[ \t]%d", &uid) > 0) break;
             }
 
-            char *real_username = getpwuid(uid)->pw_name;
-            if (strncmp(real_username, filter_user, 255) != 0) {
+            strncpy(real_username, getpwuid(uid)->pw_name, 127);
+            if (filter_user && strncmp(real_username, filter_user, 128) != 0) {
                 goto end;
             }
         }
 
         if (strstr(link_realpath, filter_execname) != NULL) {
             if (!flag_verbose) printf("%d\n", pid);
+            else if (!filter_user) printf("%d: %s (%s)\n", pid, link_realpath, real_username);
             else printf("%d: %s\n", pid, link_realpath);
         }
 
 
 end:
         if (status_file) fclose(status_file);
-        free(status_file_path);
-        free(proc_file_path);
-        free(link_realpath);
-        free(link_path);
-        free(line);
     }
+
     closedir(proc);
     
     return EXIT_SUCCESS;
